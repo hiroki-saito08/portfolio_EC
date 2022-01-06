e67733fff9d7724641c7037d476c0969e2b6133e
saitohiroki <hirokisaito0813@gmail.com>
2022-01-06 22:17:52 +0900

    バグ修正

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -38,7 +38,7 @@
                         <form method="POST" action="{{ route('user.logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-dropdown-link :href="route('user.logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
