b81f110d2f26a9a49d79033c03ce90b62cb50fc8
近重明日翔 <chikashige.8.3@gmail.com>
2022-01-07 17:33:03 +0900

    header作成

--- resources/views/layouts/appcopy.blade.php
+++ resources/views/layouts/appcopy.blade.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <title>Apparel EC</title>
   <meta name="description" content="">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
   <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
   <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,50 +2,80 @@
   <!-- ヘッダー部分 -->
     <div id="headerBox">
         <div id="top-logo">
-          <p>EXAMPLE LOGO</p>
+          <a href="/">
+            <img src="{{ asset('images/logo02.jpg') }}" alt="" width="100px" height="50px">
+          </a>
         </div>
 
-        <div>
-          <div>
+        <div id="h-category">
+          <div class="h-category-margin">
             <a href="">
               MEN'S
             </a>
           </div>
-          <div>
+
+          <div class="h-category-margin">
             <a href="">
               WOMEN'S
             </a>
           </div>
-          <div>
+
+          <div class="h-category-margin">
             <a href="">
               KID'S
             </a>
           </div>
-          <div>
+
+          <div class="h-category-margin">
             <a href="">
               ALL
             </a>
           </div>
+
         </div>
 
         <div id="searchBox">
-            <form action="search" class="searchform">
-                <div class="form-example">
-                  <input type="submit" value="検索">
-                </div>
-                <input type="text" name="words" id="words" placeholder="キーワード検索" required>
+            <form action="search" id="searchform">
+              <input type="text" name="words" id="words" placeholder="キーワード検索" required>
             </form>
+            <div class="form-example">
+              <input class="logo-img" type="image" src="{{ asset('images/search.png') }}">
+            </div>
         </div>
+
+        <div id="h-r-box">
+          <div id="header-right">
+            <div class="h-img-right">
+              <a href="">
+                <img class="logo-img" src="{{ asset('images/home.png') }}" alt="" >
+              </a>
+            </div>
+
+          <div class="h-img-right">
+              <a href="">
+                <img class="logo-img" src="{{ asset('images/hart.png') }}" alt="" >
+              </a>
+          </div>
+
+          @auth
+          <div class="h-img-right">
+            <a href="">
+              <img class="logo-img" src="{{ asset('images/account02.png') }}" alt="" >
+            </a>
+          </div>
+
+          @else
+          <div class="h-img-right">
+            <a href="">ログイン</a>
+          </div>
+
+          <div class="h-img-right">
+            <a href="">新規登録</a>
+          </div>
+
+          @endauth
+        </div>
+      </div>
     </div>
 
-  <ul>
-    <li class="nav-item"><a href="">ホーム</a></li>
-    <li class="nav-item"><a href="">お気に入り</a></li>
-    @auth
-    <li class="nav-item"><a href="">ログアウト</a></li>
-    @else
-    <li class="nav-item"><a href="">ログイン</a></li>
-    <li class="nav-item"><a href="">新規登録</a></li>
-    @endauth
-  </ul>
 </header>
