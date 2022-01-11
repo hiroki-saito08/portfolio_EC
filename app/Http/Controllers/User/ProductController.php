5c2a95350a0e63a99c07339ce86791fd46907010
saitohiroki <hirokisaito0813@gmail.com>
2022-01-11 23:53:08 +0900

    topbladeの枠作成

--- app/Http/Controllers/User/ProductController.php
+++ app/Http/Controllers/User/ProductController.php
@@ -19,17 +19,10 @@ class ProductController extends Controller
         // 全ての商品
         $all_products = Product::all();
 
-        // WOMENSに表示する商品を絞る機能
-        $womens_products = Product::where('gender', 'mens_and_womens')->orWhere('gender', 'womens')->get();
-
-        // MENSに表示する商品を絞る機能
-        $mens_products = Product::where('gender', 'mens_and_womens')->orWhere('gender', 'mens')->get();
-
-        // KIDSに表示する商品を絞る機能
-        $kids_products = Product::where('category', 'kids')->get();
-
-        // TRENDING NOWに表示する商品を絞る機能
-        $trending_products = Product::where('category', 'trend')->get();
+        // 商品数
+        // $products_count = Product::count();
+        // ランダム抽出
+        // $randomProduct = Product::inRandomOrder()->take($products_count)->get();
 
         return view('user.top', compact('user', 'all_products'));
     }
--- resources/views/user/top.blade.php
+++ resources/views/user/top.blade.php
@@ -6,8 +6,47 @@
 <div>
 <a href="{{ route('user.products') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">商品一覧へ</a>
 </div>
-<br>
-    <div id="">キープ表示</div>
+<div id=top_header>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+</div>
+<div id=top_trending>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+  <div>
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+  </div>
+</div>
+<div class="big_image">
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+</div>
+<div class="shop_by_category">
+  SHOP BY CATEGORY
+</div>
+<div class="big_image">
+    <img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません">
+</div>
+<div class="shop_by_category">
+  SHOP BY CATEGORY
+</div>
+<div class="footer">
+  footer
+</div>
+    {{-- <div id="">キープ表示</div>
   <div id="keep-images">
     <a href="">
       <!-- 画像パス -->
@@ -17,6 +56,5 @@
   <div id="keep-name">
   </div>
   <div id="keep-price">
-</div>
-
+</div> --}}
 @endsection
