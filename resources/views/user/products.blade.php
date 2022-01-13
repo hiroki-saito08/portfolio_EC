354dc2b1aae0501d55450bd9e6982fa0003c8914
saitohiroki <hirokisaito0813@gmail.com>
2022-01-14 00:03:29 +0900

    編集

--- resources/views/user/products.blade.php
+++ resources/views/user/products.blade.php
@@ -16,15 +16,14 @@
     @foreach($all_products as $product)
       <div id="product-name">
         <div>
-          <a href="">
             <!-- 画像パス -->
             <img src="{{ asset('/storage/'.$product -> image_path) }}" alt="画像が登録されてません">
-          </a>
         </div>
         <div> 商品名： {{ $product -> name }}</div>
         <div> 値段： {{ $product -> price }}</div>
         <div> サイズ： {{ $product -> size }}</div>
 
+        {{-- ここをモーダルで表示するイメージ --}}
         <div class="pop_area">
           <div class="pop_top">
             <img src="{{ asset('/storage/'.$product -> image_path) }}" alt="画像が登録されてません">
