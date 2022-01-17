278f468ade289e0825a5af153ac3b7cce762f57e
近重明日翔 <chikashige.8.3@gmail.com>
2022-01-17 15:12:28 +0900

    topページ完成

--- resources/views/layouts/appcopy.blade.php
+++ resources/views/layouts/appcopy.blade.php
@@ -6,9 +6,11 @@
   <title>Apparel EC</title>
   <meta name="description" content="">
   <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
   <link rel="stylesheet" href="{{ asset('css/products.css') }}">
   <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
   <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
   <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -20,6 +22,7 @@
     <main>
       @yield('content')
     </main>
+    @include('layouts/footer')
   </div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
--- resources/views/user/top.blade.php
+++ resources/views/user/top.blade.php
@@ -4,25 +4,27 @@
 
 <!-- main image部分 -->
 <!-- ↓スライダー本体 -->
-<ul class="thumb-item">
-     <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model5.jpg') }}" alt="画像が登録されてません"></a></li>
-     <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model2.jpg') }}" alt="画像が登録されてません"></a></li>
-     <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model3.jpg') }}" alt="画像が登録されてません"></a></li>
-     <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model4.jpg') }}" alt="画像が登録されてません"></a></li>
-     <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model1.jpg') }}" alt="画像が登録されてません"></a></li>
-</ul>
-<!-- ↓サムネイル -->
-<!-- <div id="slider-box"> -->
-  <ul class="thumb-item-nav">
-      <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model5.jpg') }}" alt="画像が登録されてません"></a></li>
-      <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model2.jpg') }}" alt="画像が登録されてません"></a></li>
-      <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model3.jpg') }}" alt="画像が登録されてません"></a></li>
-      <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model4.jpg') }}" alt="画像が登録されてません"></a></li>
-      <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model1.jpg') }}" alt="画像が登録されてません"></a></li>
+<div id="slider-box">
+  <ul class="thumb-item">
+       <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model5.jpg') }}" alt="画像が登録されてません"></a></li>
+       <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model2.jpg') }}" alt="画像が登録されてません"></a></li>
+       <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model3.jpg') }}" alt="画像が登録されてません"></a></li>
+       <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model4.jpg') }}" alt="画像が登録されてません"></a></li>
+       <li><a href=""><img class="slider-main" src="{{ asset('/storage/'.'model1.jpg') }}" alt="画像が登録されてません"></a></li>
   </ul>
+  <!-- ↓サムネイル -->
+  <!-- <div id="slider-box"> -->
+    <ul class="thumb-item-nav">
+        <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model5.jpg') }}" alt="画像が登録されてません"></a></li>
+        <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model2.jpg') }}" alt="画像が登録されてません"></a></li>
+        <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model3.jpg') }}" alt="画像が登録されてません"></a></li>
+        <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model4.jpg') }}" alt="画像が登録されてません"></a></li>
+        <li><a href="#"><img class="slider-img" src="{{ asset('/storage/'.'model1.jpg') }}" alt="画像が登録されてません"></a></li>
+    </ul>
+</div>
 <!-- </div> -->
 
-<div>
+<!-- <div> -->
 <a href="{{ route('user.products') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">商品一覧へ</a>
 [トップページです]
 
@@ -35,39 +37,23 @@
 
 <div id="main_bar"></div>
 
-<div id=top_header>
+<div id="main-box">
   <div id="top_img_box">
-    <a href="{{ route('user.products') }}"><img class="top_main_img" src="{{ asset('/storage/'.'onepiece3.jpg') }}" alt="画像が登録されてません"></a>
+    <a href="{{ route('user.products') }}">
+      <img class="top_main_img" src="{{ asset('/storage/'.'onepiece3.jpg') }}" alt="画像が登録されてません">
+    </a>
   </div>
 
   <div id="main_bar"></div>
 
   <div id="top_img_box">
-    <a href="{{ route('user.products') }}"><img class="top_main_img" src="{{ asset('/storage/'.'T2.jpg') }}" alt="画像が登録されてません"></a>
+    <a href="{{ route('user.products') }}">
+      <img class="top_main_img" src="{{ asset('/storage/'.'model7.jpg') }}" alt="画像が登録されてません">
+    </a>
   </div>
+</div>
 
-  <div id="main_bar"></div>
-  <!-- <div>
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-  </div>
-</div>
-<div id=top_trending>
-  <div>
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-  </div>
-  <div>
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-  </div>
-  <div>
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-  </div>
-  <div>
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-  </div>
-</div>
-<div class="big_image">
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'Tシャツ.jpeg') }}" alt="画像が登録されてません"></a>
-</div> -->
+<div id="main_bar"></div>
 
 <div class="shop_by_category">
   <div class="shop_category_box">
@@ -76,14 +62,17 @@
     </div>
 
     <div id="category_font_box">
-    <p id="category_font_p">We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.</p>
+      <p id="category_font_p">We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.
+      </p>
     </div>
   </div>
 
   <div id="main_bar"></div>
 
   <div id="big_image">
-    <a href="{{ route('user.products') }}"><img src="{{ asset('/storage/'.'jacket1.jpg') }}" alt="画像が登録されてません"></a>
+    <a href="{{ route('user.products') }}">
+      <img src="{{ asset('/storage/'.'jacket1.jpg') }}" alt="画像が登録されてません">
+    </a>
   </div>
 
   <div id="main_bar"></div>
@@ -97,9 +86,8 @@
     <p id="category_font_p">We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.</p>
     </div>
   </div>
-
-<div class="footer">
-  footer
 </div>
+
+<div id="main_bar"></div>
 
 @endsection
