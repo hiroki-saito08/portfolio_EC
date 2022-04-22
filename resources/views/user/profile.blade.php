8c817e55fac476defa42e6d0bffa1913e01f67f9
saitohiroki <hirokisaito0813@gmail.com>
2022-04-22 09:41:14 +0900

    購入画面、お気に入り画面のレイアウト崩れ修正

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,8 +4,8 @@
 
   <div id="contents">
     <div id="contents-box">
-      <div class="">
-        <h1 class="">ユーザー情報編集</h1>
+      <div>
+        <h1>ユーザー情報編集</h1>
       </div>
 
         {{-- フラッシュメッセージ --}}
@@ -14,46 +14,35 @@
         {{ session('message') }}
         </div>
         @endif
-      <div class="">
+      <div>
         <form method="POST" action="{{ route('user.update', $user->id) }}">
           @csrf
-            <div class="">
+            <div>
               <div class="relative">
-                <label for="name" class="">ユーザーネーム</label>
+                <label for="name">ユーザーネーム</label>
                 <input type="text" id="name" name="name" value="{{ $user->name }}" required class="">
               </div>
             </div>
-            <div class="">
+            <div>
               <div class="relative">
-                <label for="email" class="">メールアドレス</label>
-                <input type="email" id="email" name="email" value="{{ $user->email }}" required class="">
+                <label for="email">メールアドレス</label>
+                <input type="email" id="email" name="email" value="{{ $user->email }}" required>
               </div>
             </div>
-          </div>
-          <div class="">
-            <div class="relative">
-              <label for="address" class="">住所を登録する</label>
-              <input type="text" id="address" name="address" value="{{ $user->address }}" class="">
-            </div>
-          </div>
-          <div class="">
-            <div class="relative">
-              <label for="password" class="">パスワード</label>
-              <input type="password" id="password" name="password" required class="">
-            </div>
-          </div>
 
-          <a href="">
+        <div class="relative">
+          <label for="password">パスワード</label>
+          <input type="password" id="password" name="password" required >
+        </div>
+
             <button type="submit" id="redirect-button">更新する</button>
-          </a>
-          <div class="">
-            <a href="{{ route('user.top') }}">
-            <button type="submit" id="redirect-button">TOPに戻る</button>
-            </a>
-          </div>
         </form>
+        <div>
+          <a href="{{ route('user.top') }}">
+          <button type="submit" id="redirect-button">TOPに戻る</button>
+          </a>
+        </div>
       </div>
-
     </div>
   </div>
 @endsection
